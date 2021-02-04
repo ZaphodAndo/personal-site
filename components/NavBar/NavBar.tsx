059938f24fcba855ff57960e51b93970e114c913
Ethan <ethan.anderson00@gmail.com>
2021-02-04 16:53:08 +0000

    Updated NavBar to change active state

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./NavBar.module.css";
 
 function NavBar() {
+  const router = useRouter();
+  const style = {
+    backgroundColor: "var(--secondary-accent-colour)"
+  };
+
+  const homeClick = () => {
+    router.push("/");
+  };
+
+  const postsClick = () => {
+    router.push("/posts");
+  };
+
   return (
     <nav className={styles.nav}>
-      <Link href="/">Home</Link>
-      <Link href="/posts">Posts</Link>
+      <a onClick={homeClick} style={router.pathname === "/" ? style : null}>
+        Home
+      </a>
+      <a onClick={postsClick} style={router.pathname === "/posts" ? style : null}>
+        Posts
+      </a>
     </nav>
   );
 }
