fe2b8ec1eb2c0ad079e33a747020964cec3bf1bc
Ethan <ethan.anderson00@gmail.com>
2021-02-08 20:28:43 +0000

    Added content to timeline

---
+++ components/Icons/PostIcon.tsx
@@ -0,0 +1,20 @@
+function PostIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      stroke="#E0E0E0"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-edit-2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+    </svg>
+  );
+}
+
+export default PostIcon;
--- components/TimeLinePost/TimeLinePost.tsx
+++ components/TimeLinePost/TimeLinePost.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
+import PostIcon from "../Icons/PostIcon";
 import styles from "./TimeLinePost.module.css";
 
 function TimeLinePost({ post }) {
   return (
-    <div>
-      <Link as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`} href="/posts/[slug]">
-        {post.data.title}
-      </Link>
+    <div className={styles.post}>
+      <div className={styles.side}>
+        <div className={styles.icon}>
+          <PostIcon />
+        </div>
+        <div className={styles.divider} />
+      </div>
+      <div className={styles.content}>
+        <p className={styles.info}>New Post - {post.data.date}</p>
+        <Link as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`} href="/posts/[slug]">
+          <div className={styles.card}>
+            <p className={styles.header}>{post.data.title}</p>
+            <p className={styles.desc}>{post.data.description}</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
